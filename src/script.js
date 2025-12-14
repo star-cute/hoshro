7fac1d1d15f1ba10a0c8752c88bcf57c39310e89
star-cute <mseyedipour@gmail.com>
2025-12-14 11:40:27 +0330

    Initial commit

--- src/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuBtn = document.getElementById("menu-btn");
-    const mobileMenu = document.getElementById("mobile-menu");
-
-    menuBtn.addEventListener("click", () => {
-        mobileMenu.classList.toggle("hidden");
-    });
-});
