5d5be6da7f742ba01f8beadd0210d810eb2b57aa
star-cute <mseyedipour@gmail.com>
2025-12-14 09:04:23 +0330

    hoshro

---
+++ src/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuBtn = document.getElementById("menu-btn");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    menuBtn.addEventListener("click", () => {
+        mobileMenu.classList.toggle("hidden");
+    });
+});
